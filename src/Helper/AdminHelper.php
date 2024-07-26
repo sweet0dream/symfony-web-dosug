4c50a1e9f35976e3a9be18dc11f46d4797939721
Kalinin Kirill <kalinin.k@optimax.dev>
2024-07-26 23:31:12 +0400

    admin module

--- src/Helper/AdminHelper.php
+++ src/Helper/AdminHelper.php
@@ -24,7 +24,7 @@ class AdminHelper {
     {
         $premiumItems = $this->em->getRepository(ItemStatus::class)->findBy(
             ['premium' => true],
-            ['premium_priority' => 'ASC']
+            ['premium_priority' => 'RAND()']
         );
 
         foreach ($premiumItems as $item) {
