76044f38b3f25f0f712f8937d58cea3be70af0db
sweet0dream <sweet0dream@proton.me>
2024-10-04 04:37:55 +0300

    optimize: load fonts fix

--- assets/app.js
+++ assets/app.js
@@ -5,4 +5,3 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'https://kit.fontawesome.com/21a3382279.js';
 import './styles/app.css';
-import 'https://fonts.googleapis.com/css2?family=Cuprum:ital,wght@0,400..700;1,400..700&family=Great+Vibes&family=Roboto+Slab:wght@100..900&display=swap';
